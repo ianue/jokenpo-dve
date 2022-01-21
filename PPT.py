23a9016b1c32bd95a7d9c2235151a0a28d84ab3f
Ian Carlos <ianjf1997@gmail.com>
2022-01-21 19:28:21 -0300

    Primeiro commit

---
+++ PPT.py
@@ -0,0 +1,12 @@
+import random
+pedra = str.upper('Pedra')
+papel = str.upper('Papel')
+tesoura = str.upper('Tesoura')
+print ('Escolha : {} | {} | {} '.format(pedra, papel, tesoura))
+escolha = str.upper(input('Qual é a sua opção? '))
+ep = ["Pedra", "Papel", "Tesoura"]
+me = str.upper(random.choice(ep))
+print('=========================================')
+print('Você escolheu : {} '.format(escolha))
+print('Cierri Seti Careca escolheu : {} '.format(me))
+print('|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|')
